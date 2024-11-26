ecbfd4298fb74358076fb4ca942f392f9ed2152b
Trần Kim Vinh <trndinhh1905@gmail.com>
2024-11-27 00:14:52 +0700

    Add files via upload

---
+++ FootballZombie.java
@@ -0,0 +1,18 @@
+package Zoombie;
+
+public class FootballZombie extends Zombie {
+        private int speed = 2;
+
+        public FootballZombie(int y) {
+            super(y);
+            setHealth(500);
+            setDamage(75);
+            loadImage("/images/zombies/football_zombie.gif");
+            getImageDimensions();
+        }
+
+        public void move() {
+            locX -= speed;
+        }
+    }
+
