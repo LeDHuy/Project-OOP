21b239353d5a0aa2a5f9518d9c9e7d4479db07d9
Trần Kim Vinh <trndinhh1905@gmail.com>
2024-12-08 20:21:07 +0700

    Update NormalZombie.java

--- PlantsVsZombies/src/NormalZombie.java
+++ PlantsVsZombies/src/NormalZombie.java
@@ -1,9 +1,17 @@
 /**
- * Created by Armin on 6/25/2016.
+ * Represents a normal/basic zombie type in the game
+ * Inherits from the base Zombie class
+ * This is the standard zombie with default health and speed values
  */
 public class NormalZombie extends Zombie {
-
+/**
+     * Constructor to create a new normal zombie
+     * 
+     * @param parent The GamePanel that contains this zombie
+     * @param lane The lane number where this zombie will move
+     */
     public NormalZombie(GamePanel parent,int lane){
+        // Apply the parameters that are provided to invoke the constructor of the parent class (Zombie).
         super(parent,lane);
     }
 
