00c544735dc0098a93f6a1ef5dee2d7d2830a27e
Trần Kim Vinh <trndinhh1905@gmail.com>
2024-12-08 21:57:16 +0700

    Update Peashooter.java

--- PlantsVsZombies/src/Peashooter.java
+++ PlantsVsZombies/src/Peashooter.java
@@ -1,27 +1,30 @@
 import java.awt.event.ActionEvent;
 import javax.swing.*;
 
-/**
- * Created by Armin on 6/25/2016.
- */
 public class Peashooter extends Plant {
-
     public Timer shootTimer;
-
-
+    // A clock to manage the frequency of shots
     public Peashooter(GamePanel parent,int x,int y) {
+        // Call parent Plant class constructor
         super(parent,x,y);
+        // Set up a timer to go off every two seconds, or 2000 milliseconds.
         shootTimer = new Timer(2000,(ActionEvent e) -> {
             //System.out.println("SHOOT");
+            // Only shoot if there are zombies in the same lane
             if(gp.laneZombies.get(y).size() > 0) {
+                // Add a new pea to the lane
+                // Position is calculated as 103 + (plant's x position * 100)
                 gp.lanePeas.get(y).add(new Pea(gp, y, 103 + this.x * 100));
             }
         });
+        // Set the timer for the shot.
         shootTimer.start();
     }
-
+    // Override the stop method from Plant class
     @Override
+    
     public void stop(){
+        // Stop the shooting timer
         shootTimer.stop();
     }
 
