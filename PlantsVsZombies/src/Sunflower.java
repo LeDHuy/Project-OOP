c286c3739db0a06d245d010ee63b2cf2ee48da34
Trần Kim Vinh <trndinhh1905@gmail.com>
2024-12-08 23:01:34 +0700

    Update Sunflower.java

--- PlantsVsZombies/src/Sunflower.java
+++ PlantsVsZombies/src/Sunflower.java
@@ -1,20 +1,26 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 
-/**
- * Created by Armin on 6/28/2016.
- */
 public class Sunflower extends Plant {
-
+    // A timer for controlling the amount of sunlight
     Timer sunProduceTimer;
-
+    // Constructor for creating a sunflower at specific coordinates
     public Sunflower(GamePanel parent,int x,int y) {
+        // Call parent Plant class constructor
         super(parent, x, y);
+        // Create timer that produces sun every 15000 milliseconds (15 seconds)
         sunProduceTimer = new Timer(15000,(ActionEvent e) -> {
+            // Create new sun object with calculated positions:
+            // X position: 60 + (plant's x position * 100)
+            // Start Y: 110 + (plant's y position * 120)
+            // End Y: 130 + (plant's y position * 120)
             Sun sta = new Sun(gp,60 + x*100,110 + y*120,130 + y*120);
+            // Add sun to active suns list
             gp.activeSuns.add(sta);
+            // Add sun to game panel with layer priority 1
             gp.add(sta,new Integer(1));
         });
+        // Start the sun production timer
         sunProduceTimer.start();
     }
 
