43c023adcbd6a8c336b6ebcaf4b7ade08297d292
Trần Kim Vinh <trndinhh1905@gmail.com>
2024-12-08 19:08:06 +0700

    Update ConeHeadZombie.java

--- ConeHeadZombie.java
+++ ConeHeadZombie.java
@@ -1,8 +1,9 @@
-
+// A particular kind of zombie called as a "ConeHeadZombie" has a traffic cone on its head.
 public class ConeHeadZombie extends Zombie {
-
+    // A constructor in a particular game lane that generates a new ConeHeadZombie
     public ConeHeadZombie(GamePanel parent,int lane){
+        // Gets the same parameters for calling the parent (Zombie) class constructor.
         super(parent,lane);
-        health = 1800;
+        health = 1800; // Improves this zombie's health to 1800, which should make it stronger than other zombies.
     }
 }
